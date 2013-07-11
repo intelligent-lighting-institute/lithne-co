625680e4cdfa2881c600a3d15879b72250da82ec
Elco Jacobs <elcojacobs@gmail.com>
2013-07-11 02:24:35 +0200

    Rewrote debug functions to make printing line end optional. Added prefix to debug function

--- lithneco/src/print.cpp
+++ lithneco/src/print.cpp
@@ -17,18 +17,28 @@ static char printBuffer[PRINT_BUFFER_SIZE];
 extern volatile uint8_t main_port_open;
 
 
-void printToPort(uint8_t port, const char * string, uint16_t numChars){	
+void printToPort(uint8_t port, bool printLine, const char * string, uint16_t numChars){	
 	if (!(main_port_open & (1 << port))) {
 		// Port not open
 		return;
 	}		
 	udi_cdc_multi_write_buf(port, string, numChars);
-	udi_cdc_multi_putc(port, '\n');
-	udi_cdc_multi_putc(port, '\r');
+	if(printLine){
+		udi_cdc_multi_putc(port, '\n');
+		udi_cdc_multi_putc(port, '\r');
+	}
+}
+
+void debugMessageImpl_P(const char *fmt, ... ){
+	va_list args;
+	printToPort(DEBUG_PORT, false, "\t\t\t\tDEBUG\t", sizeof("\t\t\t\tDEBUG\t")); // print indent and DEBUG without line end
+	va_start (args, fmt );
+	printfToPort_P(DEBUG_PORT, true, fmt, args); // print debug message and line end
+	va_end (args);
 }
 
 // create a printf like interface to each port. Format string stored in PROGMEM
-void printfToPort_P(uint8_t port, const char *fmt, ... ){	
+void printfToPort_P(uint8_t port, bool printLine, const char *fmt, ... ){	
 	uint16_t numChars = 0;
 	va_list args;
 	va_start (args, fmt );
@@ -37,11 +47,11 @@ void printfToPort_P(uint8_t port, const char *fmt, ... ){
 	if (numChars > PRINT_BUFFER_SIZE){
 		numChars = PRINT_BUFFER_SIZE;
 	}
-	printToPort(port, printBuffer, numChars);	
+	printToPort(port, printLine, printBuffer, numChars);	
 }
 
 // create a printf like interface to each port. Format string stored in RAM
-void printfToPort(uint8_t port, char *fmt, ... ){
+void printfToPort(uint8_t port, bool printLine, char *fmt, ... ){
 	uint16_t numChars = 0;
 	va_list args;
 	va_start (args, fmt );
@@ -50,5 +60,5 @@ void printfToPort(uint8_t port, char *fmt, ... ){
 	if (numChars > PRINT_BUFFER_SIZE){
 		numChars = PRINT_BUFFER_SIZE;
 	}
-	printToPort(port, printBuffer, numChars);
+	printToPort(port, printLine, printBuffer, numChars);
 }
--- lithneco/src/print.h
+++ lithneco/src/print.h
@@ -14,13 +14,14 @@
 #define DEBUG_PORT 0 // send debug messages to main serial port
 #define PRINT_BUFFER_SIZE 256 // A debug message is maximally 256 bytes
 
-#define debugMessage(string, ...) printfToPort_P(DEBUG_PORT, PSTR(string), ##__VA_ARGS__)
+#define debugMessage(string, ...) debugMessageImpl_P(PSTR(string), ##__VA_ARGS__)
 
-void printToPort(uint8_t port, const char * string, uint16_t numChars);
+void printToPort(uint8_t port, bool printLine, const char * string, uint16_t numChars);
 
-void printfToPort_P(uint8_t port, const char *fmt, ... );
+void printfToPort_P(uint8_t port, bool printLine, const char *fmt, ... );
 
-void printfToPort(uint8_t port, char *fmt, ... );
-	
+void printfToPort(uint8_t port, bool printLine, char *fmt, ... );
+
+void debugMessageImpl_P(const char *fmt, ... );
 
 #endif /* DEBUG_H_ */
