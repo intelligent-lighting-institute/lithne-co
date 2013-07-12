9eda7106d4ff9a4204f9ce5ba027663a6b2a7525
Elco Jacobs <elcojacobs@gmail.com>
2013-07-12 13:51:56 +0200

    Replaced interrupt based millis implementation with a 32-bit timer. Overflow event of TCD0 is routed to the input of TCD1 through the event system to combine them into a 32-bit timer. The timer registers are read directly and scaled to give milliseconds. Overflows in 1.5 day.

--- lithneco/src/millis.c
+++ lithneco/src/millis.c
@@ -8,18 +8,26 @@
 #include "asf.h"
 #include "millis.h"
 
-volatile unsigned long millis_count;
-
 // We don't use the Arduino init(), because it initializes system clocks, pwm clock and pins.
 // We define our own timer interrupt here.
 
 void millis_init(void){
-	PR.PRPD &= ~(PR_TC0_bm);	
-	millis_count = 0;
-	TCD0.PER   = 4000;
-	TCD0.CTRLB    = ( TCD0.CTRLB & ~TC0_WGMODE_gm ) | TC_WGMODE_NORMAL_gc;
-	TCD0.INTCTRLA = TC_OVFINTLVL_LO_gc;
-	TCD0.CTRLA = TC_CLKSEL_DIV8_gc;
+	PR.PRPD &= ~(PR_TC0_bm);	// enable TCD0 module
+	PR.PRPD &= ~(PR_TC1_bm);	// enable TCD1 module
+	PR.PRGEN &= ~(PR_EVSYS_bm);
+		
+	TCD0.PER		= 0xFFFF;
+	TCD0.CTRLB		= TC_WGMODE_NORMAL_gc;
+	TCD0.CTRLD		= 0; // no event action
+	EVSYS.CH0MUX = EVSYS_CHMUX_TCD0_OVF_gc; // set event event channel 0 to TCD0 overflow
+	
+	TCD1.PER		= 0xFFFF;
+	TCD1.CTRLB		= TC_WGMODE_NORMAL_gc;
+	TCD1.CTRLD		= TC0_EVDLY_bm | TC_EVACT_CAPT_gc; // set event action to input capture, delay event source by 1
+	
+	// start the timers by setting the clock sources
+	TCD0.CTRLA		= TC_CLKSEL_DIV1024_gc; // timer 0 clock source = 32250 Hz
+	TCD1.CTRLA		= TC_EVSEL_CH0_gc; // timer 1 clock source = event channel 0
 }
 
 void delay(unsigned long ms){
@@ -27,11 +35,8 @@ void delay(unsigned long ms){
 }
 
 unsigned long millis(void){
-	return millis_count;
+	// get 32 bit value from combination of both timers. Save as 64 bit to be able to multiply by 2
+	volatile uint64_t timers_combined = TCD1.CNT;
+	timers_combined = (timers_combined<<16) + TCD0.CNT; 
+	return ((timers_combined*2) / 65); // scale to milliseconds
 }
-
-ISR(TCD0_OVF_vect)
-{
-	++millis_count;
-}
-
